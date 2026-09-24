b7b254bd395203a842322e0e4f495cef4a3526ab
coderrrcoffee <coderrrcoffee@users.noreply.github.com>
2026-09-24 12:56:33 +0500

    Убрана верхняя полоса-объявление

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
-import { announcement } from '../../content/site.js'
 
 // При переходе на новую страницу возвращаем прокрутку наверх
 function ScrollToTop() {
@@ -22,7 +21,6 @@ export default function Layout() {
       <a className="skip-link" href="#main">
         К содержанию
       </a>
-      <p className="announcement">{announcement}</p>
       <Header />
       <main id="main">
         <Outlet />
